91b2fbc2aa6de6ac03e58f0b430a28fbfefbc4c8
NachBG <73742855+NachBG@users.noreply.github.com>
2020-12-03 20:24:13 +0100

    Add files via upload

--- EJERCICIOS JAVASCRIPT/Ejercicios eventos/Ejercicio2.js
+++ EJERCICIOS JAVASCRIPT/Ejercicios eventos/Ejercicio2.js
@@ -1,8 +1,36 @@
-function dragstart(capa1){
-    thus.style.opacity = '0.5';
+function start(){
+    document.getElementById("capa1") .style.opacity = '0.5';
 }
-var capes = document.querySelectorAll('#capa1');
-[].forEach.call(capes, function(capa) {
-    capa.addEventListener('dragstart', dragstart, false);
-};
-    
+
+function dragleave(){
+
+    var x = document.getElementById("capa1").style.opacity = "100%";
+}
+
+function cambioColor(){
+   
+    document.getElementById("capa2").style.background= "red";
+}
+
+function dragrelease(){
+
+    document.getElementById("capa2").style.background= "white";
+}
+
+function ondrag(capa){
+    capa.preventDefault();
+}
+
+function final(){
+    document.getElementById("capa1").remove();
+    document.getElementById("capa2").style.background="yellow";
+    document.getElementById("capa2").innerHTML="Lo has logrado!!";
+}
+
+document.getElementById("capa1").addEventListener("drag", start, false);
+document.getElementById("capa1").addEventListener("dragend",dragleave,false);
+
+document.getElementById("capa2").addEventListener("dragenter",cambioColor,false);
+document.getElementById("capa2").addEventListener("dragleave",dragrelease,false);
+document.getElementById("capa2").addEventListener("dragover",ondrag,false);
+document.getElementById("capa2").addEventListener("drop",final,false);
